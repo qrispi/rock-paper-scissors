265732aff487dbcd8badc24a273ab0e7a4d4a448
Christopher Baum <christopherabaum@gmail.com>
2023-01-12 11:04:38 -0600

    feat: Add algorithm to evaluate game state and helper methods

--- js/game.js
+++ js/game.js
@@ -1,14 +1,15 @@
 class Game {
-    constructor(firstPlayer, secondPlayer) {
-        this.player1 = new Player(firstPlayer)
-        this.player2 = new Player(secondPlayer)
+    constructor(humanPlayer, computerPlayer) {
+        this.human = new Player(humanPlayer)
+        this.computer = new Player(computerPlayer)
+        this.gamePieces = 3
         // How do I pass the new players the appropriate parameters? Should I set the player class up to use an object as a parameter?
         // Not sure if I need this:
         // this.classicGame = true
         // this.expandedGame = false
 
-        
-        this.expandedGame = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        // this.classicPieces = ['rock', 'paper', 'scissors']
+        // this.expandedPieces = ['rock', 'paper', 'scissors', 'lizard', 'spock']
     }
 
     // Not sure if I will need this either:
@@ -16,6 +17,45 @@ class Game {
     //     this.classicGame = !this.classicGame
     //     this.expandedGame = !this.expandedGame
     // }
+    switchGames() {
+        if (this.gamePieces === 3) {
+            this.gamePieces = 5
+        } else {
+            this.gamePieces = 3
+        }
+    }
+
+    // I don't think I even need to keep track of what game is being played in the class, my method doesn't give a shit what the numbers are
+    // I guess I might need it to tell the DOM which icons to display
+    playGame() {
+        // function fight(num) {
+        var playerChoice = this.human.takeTurn()
+        var computerChoice = this.computer.takeTurn(this.gamePieces)
+        var sum = (playerChoice - computerChoice)
+        if (sum === 0) {
+            return 'draw'
+        } else {
+            return ((sum > 0 && sum % 2 === 0) || (sum < 0 && sum % 2 !== 0)) 
+        } 
+    }
+
+    // This should go in main.js to manipulate the DOM
+    showWinner() {
+        if (this.playGame() === 'draw') {
+            return "It's a draw"
+        }
+        if (!this.playGame()) {
+            return "Computer Wins!"
+        }
+        if (this.playGame()) {
+            return "Human Wins!"
+        }
+    }
+
+    // Not sure if this will work as intended
+    resetBoard() {
+        return new Game(this.human, this.computer)
+    }
 }
 
 
