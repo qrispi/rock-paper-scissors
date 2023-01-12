0b2b212e50cc3b0ca5e5df0459dae7d89de441c8
Christopher Baum <christopherabaum@gmail.com>
2023-01-12 13:21:25 -0600

    refac: Clean up player.js

--- js/player.js
+++ js/player.js
@@ -3,38 +3,14 @@ class Player {
         this.name = playerObject.name
         this.token = playerObject.token
         this.wins = playerObject.wins
-        // this.possiblePieces = ['rock', 'paper', 'scissors', 'lizard', 'spock']
     }
 
     takeTurn(num) {
         if (num) {
             return Math.floor(Math.random() * num)
         } else {
-            // 'index number of icon that player clicked'
-            return 0
+            // 'num = index number of icon that player clicked'
+            return num || 0
         }
     }
-
-    // computerRandom(num) {
-    //     return Math.floor(Math.random() * num)
-    // }
 }
-
-
-// A player.js file that contains a Player class.
-// Player methods must include, but are not limited to:
-// constructor - properties should include: name (ex: 'Human'), token (ex: '👩🏻'), wins (ex: 0)
-// saveWinsToStorage - only necessary if you choose the localStorage extension
-// retrieveWinsFromStorage - only necessary if you choose the localStorage extension
-// takeTurn
-// A game.js file that contains a Game class.
-// A Game should include:
-// Two Player instances
-// A way to keep track of the data for the game board
-// A way to keep track of the selected game type
-// A way to check the Game’s board data for win conditions
-// A way to detect when a game is a draw (no one has won)
-// A way to reset the Game’s board to begin a new game
-
-{name: human, wins: 0,}
-{name: computer, wins: 0,}
