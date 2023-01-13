ec170410997498e24096c43cfd85a0c151a62b92
Christopher Baum <christopherabaum@gmail.com>
2023-01-13 15:53:52 -0600

    feat: Add vars and listeners for game rules

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var bullpenRules = document.getElementById('#bullpen-rules')
+var annexRules = document.getElementById('#annex-rules')
+
+bullpenRules.addEventListener('click', startSimpleGame)
+annexRules.addEventListener('click', startHardGame)
+
+function startSimpleGame() {
+
+}
+
+function startHardGame() {
+    
+}
