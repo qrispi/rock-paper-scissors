a4c5295688a4f74c67ab4da79bf9c8c4af6ff6c5
Christopher Baum <christopherabaum@gmail.com>
2023-01-14 16:52:02 -0600

    refac: Update fighter objects, win messages and resetBoard method

--- js/game.js
+++ js/game.js
@@ -3,11 +3,11 @@ class Game {
         this.human = new Player(humanPlayer)
         this.computer = new Player(computerPlayer)
         this.fighters = [
-            {imgSrc: "assets/michael.png", alt: "Michael Scott", index: 0, class: "michael", winCondition: ["embarrases Jim", "& calms Angela"]},
-            {imgSrc: "assets/dwight.png", alt: "Dwight Schrute", index: 1, class: "dwight", winCondition: ["wrestles Michael", "& intimidates Toby"]},
-            {imgSrc: "assets/jim.png", alt: "Jim Halpert", index: 2, class: "jim", winCondition: ["pranks Dwight", "& throws/exposes Angela"]},
-            {imgSrc: "assets/toby.png", alt: "Toby Flenderson", index: 3, class: "toby", winCondition: ["bores Jim", "& uses HR on Michael"]},
-            {imgSrc: "assets/angela.png", alt: "Angela Martin", index: 4, class: "angela", winCondition: ["seduces Dwight", "& insults Toby"]}]
+            {imgSrc: "assets/michael.png", alt: "Michael Scott", index: 0, name: "Michael", winCondition: ["embarrases Jim", "calms Angela"]},
+            {imgSrc: "assets/dwight.png", alt: "Dwight Schrute", index: 1, name: "Dwight", winCondition: ["wrestles Michael", "intimidates Toby"]},
+            {imgSrc: "assets/jim.png", alt: "Jim Halpert", index: 2, name: "Jim", winCondition: ["pranks Dwight", "throws/exposes Angela"]},
+            {imgSrc: "assets/toby.png", alt: "Toby Flenderson", index: 3, name: "Toby", winCondition: ["bores Jim", "uses HR on Michael"]},
+            {imgSrc: "assets/angela.png", alt: "Angela Martin", index: 4, name: "Angela", winCondition: ["seduces Dwight", "insults Toby"]}]
     }
 
     playGame() {
@@ -22,20 +22,24 @@ class Game {
     showWinner() {
         var outcome = this.playGame()
         if (outcome === 'draw') {
-            return "It's a draw"
+            return "It's a draw!"
         }
         if (!outcome) {
             this.computer.wins++
-            return "Computer Wins!"
+            this.winner = this.computer.fighter
+            // this.loser = this.human.fighter
+            return "Better Luck Next Time... Computer Wins!"
         }
         if (outcome) {
             this.human.wins++
-            return "Human Wins!"
+            this.winner = this.human.fighter
+            // this.loser = this.computer.fighter
+            return "WooHoo!! You Win!"
         }
     }
 
     // Seems to work as intended, product of this method will need to be stored in a global var of currentGame
     resetBoard() {
-        return new Game(this.human, this.computer)
+        currentGame = new Game(this.human, this.computer)
     }
 }
