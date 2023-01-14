07fa63ec09d8b4c1102101e7c62bf2857822df44
Christopher Baum <christopherabaum@gmail.com>
2023-01-14 14:28:59 -0600

    feat: Start chooseFighter and announceWinner functions and add event listeners to player icons

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@ var bullpenRules = document.getElementById('bullpen-rules')
 var annexRules = document.getElementById('annex-rules')
 
 var chooseText = document.getElementById('choose-text')
+var winMessage = document.querySelector('.win-message')
 
 var bullpenFighters = document.querySelector('.bullpen-fighters')
 var annexFighters = document.querySelector('.annex-fighters')
 
+// var michael = document.querySelector('.michael')
+// var jim = document.querySelector('.jim')
+// var dwight = document.querySelector('.dwight')
+// var toby = document.querySelector('.toby')
+// var angela = document.querySelector('.angela')
+
 bullpenRules.addEventListener('click', startSimpleGame)
 annexRules.addEventListener('click', startHardGame)
+
+bullpenFighters.addEventListener('click', chooseFighter)
+annexFighters.addEventListener('click', chooseFighter)
+
+// michael.addEventListener('click', chooseFighter)
+// jim.addEventListener('click', chooseFighter)
+// dwight.addEventListener('click', chooseFighter)
+// toby.addEventListener('click', chooseFighter)
+// angela.addEventListener('click', chooseFighter)
 
 function startSimpleGame() {
     hide(annexRules)
@@ -23,6 +39,33 @@ function startHardGame() {
     show(annexFighters)
     chooseText.innerText = 'Choose Your Fighter!'
     currentGame.fighterQuantity = 5
+}
+
+function chooseFighter(event) {
+    hide(chooseText)
+    if (currentGame.fighterQuantity > 3) {
+        hide(annexFighters)
+        hide(annexRules)
+    } else {
+        hide(bullpenFighters)
+        hide(bullpenRules)
+    }
+    currentGame.human.fighterIndex = parseInt(event.target.closest('img').dataset.indexNumber)
+    
+    // console.log('human index: ', currentGame.human.fighterIndex)
+    var outcome = currentGame.playGame()
+    console.log(outcome)
+    announceWinner()
+}
+
+function announceWinner() {
+    show(winMessage)
+    winMessage.innerHTML = `
+        <h4>WooHoo! You Win!</h4>
+        <h5>Jim pranks Dwight</h5>
+        <img src="assets/jim.png" alt="Jim Halpert head icon" data-index-number="2">
+        <img src="assets/dwight.png" alt="Dwight Schrute head icon" data-index-number="1"></img>`
+    
 }
 
 function hide(element) {
