99b0b851dc1ceaa6cb23e0c84d93fd9f74f9d74c
Christopher Baum <christopherabaum@gmail.com>
2023-01-15 13:13:10 -0600

    feat: Add loser properties for constructor and update fighter win messages

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ class Game {
         this.fighters = [
             {imgSrc: "assets/michael.png", alt: "Michael Scott", index: 0, name: "Michael", winCondition: ["embarrases Jim", "calms Angela"]},
             {imgSrc: "assets/dwight.png", alt: "Dwight Schrute", index: 1, name: "Dwight", winCondition: ["wrestles Michael", "intimidates Toby"]},
-            {imgSrc: "assets/jim.png", alt: "Jim Halpert", index: 2, name: "Jim", winCondition: ["pranks Dwight", "throws/exposes Angela"]},
+            {imgSrc: "assets/jim.png", alt: "Jim Halpert", index: 2, name: "Jim", winCondition: ["pranks Dwight", "exposes Angela's secret"]},
             {imgSrc: "assets/toby.png", alt: "Toby Flenderson", index: 3, name: "Toby", winCondition: ["bores Jim", "uses HR on Michael"]},
             {imgSrc: "assets/angela.png", alt: "Angela Martin", index: 4, name: "Angela", winCondition: ["seduces Dwight", "insults Toby"]}]
     }
@@ -28,13 +28,13 @@ class Game {
         if (!outcome) {
             this.computer.wins++
             this.winner = this.computer.fighter
-            // this.loser = this.human.fighter
+            this.loser = this.human.fighter
             return "Better Luck Next Time... Computer Wins!"
         }
         if (outcome) {
             this.human.wins++
             this.winner = this.human.fighter
-            // this.loser = this.computer.fighter
+            this.loser = this.computer.fighter
             return "WooHoo!! You Win!"
         }
     }
